718665a71436f766bf40ae5ea0bfad3b40982fd5
szuletett <szuletett@gmail.com>
2014-12-06 17:58:13 +0100

    basic views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'events#index'
+
   
   resources :events do
     resources :reservations, only: [:index, :new, :create]
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,18 @@ Client.create!(
                name: "Client 1"
                )
 
+
+Event.create!(
+              name: "TEDx Talk",
+              time: (Time.now + 1.days),
+              )
+              
+Event.create!(
+              name: "Hotel talk",
+              time: (Time.now + 2.days),
+              )
+              
+Event.create!(
+              name: "Conference",
+              time: (Time.now + 3.days),
+              )
